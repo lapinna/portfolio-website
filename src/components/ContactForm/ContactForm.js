ae13d416e0dd41100f4c87c53cedd58bec91fb92
Inese Lapina <lapinna.inese@gmail.com>
2023-01-09 17:14:14 +0200

    adjust functionality of contact form

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -3,31 +3,46 @@ import { useRef } from "react";
 import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
-    const form = useRef();
-  
-    const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs.sendForm('Leafy.io', 'template_4gx6wtp', form.current, 'bqq819FhBxf93jTo6')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-    };
-  
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "Leafy.io",
+        "template_4gx6wtp",
+        form.current,
+        "bqq819FhBxf93jTo6"
+      )
+      .then((result) => {
+        try {
+          form.current.reset();
+          console.log(result.text);
+        } catch (error) {
+          console.log(error.text);
+        }
+      });
+    alert(`Email was successfully sent!`);
+  };
   return (
     <div className="form">
       <form ref={form} onSubmit={sendEmail}>
         <label>Your Name</label>
-        <input type="text" name="user_name"/>
+        <input type="text" name="user_name" />
         <label>Email </label>
-        <input type="email" name="user_email"/>
+        <input type="email" name="user_email" />
         <label>Subject</label>
-        <input type="text" name="message_subject"/>
+        <input type="text" name="message_subject" />
         <label>Message</label>
-        <textarea rows="6" placeholder="Type Your Message Here" name="message"/>
-        <button className="btn" type="submit">Submit</button>
+        <textarea
+          rows="6"
+          placeholder="Type Your Message Here"
+          name="message"
+        />
+        <button className="btn" type="submit">
+          Submit
+        </button>
       </form>
     </div>
   );
