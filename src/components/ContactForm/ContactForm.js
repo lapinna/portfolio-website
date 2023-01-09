d1f087eabfeed18674fa6841a9135df6d8a849dd
Inese Lapina <lapinna.inese@gmail.com>
2023-01-09 16:52:07 +0200

    add emailjsto contact form

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,18 +1,33 @@
 import "./contactForm.css";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
+    const form = useRef();
+  
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('Leafy.io', 'template_4gx6wtp', form.current, 'bqq819FhBxf93jTo6')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+  
   return (
     <div className="form">
-      <form>
+      <form ref={form} onSubmit={sendEmail}>
         <label>Your Name</label>
-        <input type="text" />
+        <input type="text" name="user_name"/>
         <label>Email </label>
-        <input type="email" />
+        <input type="email" name="user_email"/>
         <label>Subject</label>
-        <input type="text" />
+        <input type="text" name="message_subject"/>
         <label>Message</label>
-        <textarea rows="6" placeholder="Type Your Message Here" />
-        <button className="btn">Submit</button>
+        <textarea rows="6" placeholder="Type Your Message Here" name="message"/>
+        <button className="btn" type="submit">Submit</button>
       </form>
     </div>
   );
