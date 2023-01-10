2ff448c907852f61fe8cda080ecce777a8b21abe
Inese Lapina <lapinna.inese@gmail.com>
2023-01-10 14:31:52 +0200

    add contact form modal alert

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,8 +1,26 @@
 import "./contactForm.css";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { Modal, Button, Box, Typography } from "@mui/material";
 import emailjs from "@emailjs/browser";
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  borderRadius: 2,
+  boxShadow: 24,
+  p: 4,
+  textAlign: "center",
+};
+
 const ContactForm = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -23,7 +41,6 @@ const ContactForm = () => {
           console.log(error.text);
         }
       });
-    alert(`Email was successfully sent!`);
   };
   return (
     <div className="form">
@@ -40,9 +57,20 @@ const ContactForm = () => {
           placeholder="Type Your Message Here"
           name="message"
         />
-        <button className="btn" type="submit">
+        <Button id="modal-btn" type="submit" onClick={handleOpen}>
           Submit
-        </button>
+        </Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Thank you! <br /> Email was successfully sent!
+            </Typography>
+          </Box>
+        </Modal>
       </form>
     </div>
   );
